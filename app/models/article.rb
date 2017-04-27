4cd91dc5cc5bcd7a25600f139ed2e439de4035ac
vi4me <https://github.com/vi4me/Read.git>
2017-04-27 06:27:24 +0300

    comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,8 @@
 class Article < ApplicationRecord
+	has_many :comments
+  validates :title, presence: true,
+                    length: { minimum: 5 }
+                    
 	validates :title, presence: true,
                     length: { minimum: 5 }
 end
